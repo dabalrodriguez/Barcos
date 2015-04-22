a09de4df059c4bfb3c232600a28cda51416b983c
dsoutofernandez <dsoutofernandez@atanasoft222.danielcastelao.org>
2015-04-22 10:25:40 +0200

    Commit 3 - Revision final de errores

---
+++ src/bol26/Bol26.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package bol26;
+
+/**
+ *
+ * @author dsoutofernandez
+ */
+public class Bol26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Boleto obj1 = new Boleto();
+        
+        obj1.setVisible(true);
+        
+    }
+    
+}
